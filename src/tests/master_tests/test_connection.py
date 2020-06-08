7cd360254bc200c56f75aef32c62bd21ba2f3b2c
Ansh Shukla <shuklaansh96@gmail.com>
2020-06-08 02:04:01 -0700

    Unit Tests

--- src/tests/master_tests/test_connection.py
+++ src/tests/master_tests/test_connection.py
@@ -1,27 +1,42 @@
 import pytest
 from master.connection import Connection, ConnectionDead
 
+
 def test_timeout():
+    # Arrange
     connection = Connection('test_id')
     connection.timer.cancel()
+    # Act
     connection.timeout()
+    # Assert
     assert(not connection.is_alive())
 
+
 def test_is_alive():
+    # Arrange
     connection = Connection('test_id')
     connection.timer.cancel()
+    # Act and Assert
     assert(connection.is_alive())
 
+
 def test_reset_timer():
+    # Arrange
     connection = Connection('test_id')
     connection.timer.cancel()
+    # Act
     connection.reset_timer()
+    # Assert
     assert(connection.timer.is_alive())
     connection.timer.cancel()
 
+
 def test_reset_timer_exception():
+    # Arrange
     connection = Connection('test_id')
     connection.timer.cancel()
+    # Assert
     connection.timeout()
     with pytest.raises(ConnectionDead):
+        # Assert
         assert connection.reset_timer()
---
+++ src/tests/master_tests/test_connection_manager.py
@@ -0,0 +1,65 @@
+import pytest
+from master.connection import Connection, ConnectionDead, ConnectionManager
+
+
+def test_add_get_connection():
+    # Arrange
+    connection_manager: ConnectionManager = ConnectionManager()
+    connection_manager.add_connection('test_id')
+    # Act
+    connection = connection_manager.get_connection('test_id')
+    connection.timer.cancel()
+    # Assert
+    assert(isinstance(connection, Connection))
+
+
+def test_get_connection_exception_no_connection():
+    # Arrange
+    connection_manager: ConnectionManager = ConnectionManager()
+    with pytest.raises(ConnectionDead):
+        # Act and Assert
+        assert connection_manager.get_connection('non_existent')
+
+
+def test_get_connection_exception_dead_connection():
+    # Arrange
+    connection_manager: ConnectionManager = ConnectionManager()
+    connection_manager.add_connection('dead_conn')
+    connection: Connection = connection_manager.get_connection('dead_conn')
+    connection.timer.cancel()
+    # Act
+    connection.timeout()
+    with pytest.raises(ConnectionDead):
+        # Assert
+        assert connection_manager.get_connection('dead_conn')
+
+
+def test_reset_connection_timer():
+    # Arrange
+    connection_manager: ConnectionManager = ConnectionManager()
+    connection_manager.add_connection('test_id')
+    connection: Connection = connection_manager.get_connection('test_id')
+    connection.timer.cancel()
+    # Act
+    connection.reset_timer()
+    # Assert
+    assert(connection.timer.is_alive())
+    connection.timer.cancel()
+
+
+def test_cleanup_connections():
+    # Arrange
+    connection_manager: ConnectionManager = ConnectionManager()
+    connection_manager.add_connection('test_conn_1')
+    conn_1 = connection_manager.get_connection('test_conn_1')
+    connection_manager.add_connection('test_conn_2')
+    conn_2 = connection_manager.get_connection('test_conn_2')
+    conn_1.timer.cancel()
+    conn_2.timer.cancel()
+    # Act
+    conn_1.timeout()
+    connection_manager.cleanup_connections()
+    # Asserts
+    with pytest.raises(ConnectionDead):
+        assert(connection_manager.get_connection('test_conn_1'))
+    assert(connection_manager.get_connection('test_conn_2'))
