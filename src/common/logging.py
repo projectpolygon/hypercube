8409f4a8beec7a13d2b1ce6f7150daed51be0ee9
Ansh Shukla <shuklaansh96@gmail.com>
2020-06-08 22:57:24 -0700

    comments

--- src/common/logging.py
+++ src/common/logging.py
@@ -17,43 +17,99 @@ SEPARATER = '*****************************************'
 class Logger:
 
     def log_error(self, message: str, end='\n'):
+        """
+        Logs error messages
+        
+        Params:
+            message: str - the message to log
+            end: str (optional) - string appended to the end of the message. Defaults to newline
+        """
         self.print_bold(f'{SEPARATER}')
         self.print_bold(f'{SET_ERROR}ERROR', end=' ')
         self.print_datetime(end='')
         print(f':\n{message}', end=end)
 
     def log_warn(self, message: str, end='\n'):
+        """
+        Logs warning messages
+        
+        Params:
+            message: str - the message to log
+            end: str (optional) - string appended to the end of the message. Defaults to newline
+        """
         self.print_bold(f'{SEPARATER}')
         self.print_bold(f'{SET_WARN}WARNING', end=' ')
         self.print_datetime(end='')
         print(f':\n{message}', end=end)
 
     def log_info(self, message: str, end='\n'):
+        """
+        Logs info messages
+        
+        Params:
+            message: str - the message to log
+            end: str (optional) - string appended to the end of the message. Defaults to newline
+        """
         self.print_bold(f'{SEPARATER}')
         self.print_bold(f'{SET_BLUE}INFO', end=' ')
         self.print_datetime(end='')
         print(f':\n{message}', end=end)
 
     def log_success(self,  message: str, header:str = 'SUCCESS', end='\n'):
+        """
+        Logs success messages
+        
+        Params:
+            message: str - the message to log
+            header: str (optional) - string for the message header tag. Defaults to 'Success'
+            end: str (optional) - string appended to the end of the message. Defaults to newline
+        """
         self.print_bold(f'{SEPARATER}')
         self.print_bold(f'{SET_GREEN}{header}', end=' ')
         self.print_datetime(end='')
         print(f':\n{message}', end=end)
 
     def log_debug(self, message: str, end='\n'):
+        """
+        Logs debug messages
+        
+        Params:
+            message: str - the message to log
+            end: str (optional) - string appended to the end of the message. Defaults to newline
+        """
         self.print_bold(f'{SEPARATER}')
         self.print_bold(f'{SET_PURPLE}DEBUG', end=' ')
         self.print_datetime(end='')
         print(f':\n{message}', end=end)
 
     def print_datetime(self, end='\n'):
+        """
+        Prints the current date time in the following format
+        [DD Mon YYYY - HH:MM:SS] ( ex: [08 Jun 2020 - 20:14:08])
+
+        Params:
+            end: str (optional) - string appended to the end of the message. Defaults to newline
+        """
         self.print_bold(
-            f'[{strftime("%d %b %Y - %H:%M:%S")}]{SET_END}', end=end)
+            f'[{strftime("%d %b %Y - %H:%M:%S")}]', end=end)
 
     def print_bold(self, message: str, end='\n'):
+        """
+        **Meant for use internally**
+
+        Prints the message in bold formatting
+
+        Params:
+            message: str - the message to print
+            end: str (optional) - string appended to the end of the message. Defaults to newline
+
+        """
         print(f'{SET_BOLD}{message}{SET_END}', end=end)
 
     def test_colours(self):
+        """
+        **Meant for internal use**
+        """
         print(f'{SET_PURPLE}PURPLE{SET_END} ', end='')
         print(f'{SET_BLUE}BLUE{SET_END} ', end='')
         print(f'{SET_GREEN}GREEN{SET_END} ', end='')
