8a1a24618cae2e958f350bb4cc8a8d12e230c2ca
Ansh Shukla <shuklaansh96@gmail.com>
2020-07-04 21:45:41 -0700

    Increase Unit Test Coverage

--- src/tests/master_tests/test_connection.py
+++ src/tests/master_tests/test_connection.py
@@ -2,41 +2,59 @@ import pytest
 from master.connection import Connection, ConnectionDead
 
 
-def test_timeout():
-    # Arrange
-    connection = Connection('test_id')
-    connection.timer.cancel()
-    # Act
-    connection.timeout()
-    # Assert
-    assert(not connection.is_alive())
+class TestConnection:
+    connection: Connection
 
+    def setup_method(self, method):
+        """
+        Before Each
+        """
+        self.connection = Connection('test_id')
+        self.connection.timer.cancel()
 
-def test_is_alive():
-    # Arrange
-    connection = Connection('test_id')
-    connection.timer.cancel()
-    # Act and Assert
-    assert(connection.is_alive())
+    def teardown_method(self, method):
+        """
+        After Each
+        """
+        self.connection.timer.cancel()
 
-
-def test_reset_timer():
-    # Arrange
-    connection = Connection('test_id')
-    connection.timer.cancel()
-    # Act
-    connection.reset_timer()
-    # Assert
-    assert(connection.timer.is_alive())
-    connection.timer.cancel()
-
-
-def test_reset_timer_exception():
-    # Arrange
-    connection = Connection('test_id')
-    connection.timer.cancel()
-    # Assert
-    connection.timeout()
-    with pytest.raises(ConnectionDead):
+    def test_timeout(self):
+        # Act
+        self.connection.timeout()
         # Assert
-        assert connection.reset_timer()
+        assert (not self.connection.is_alive())
+
+    def test_is_alive(self):
+        # Act and Assert
+        assert (self.connection.is_alive())
+
+    def test_reset_timer(self):
+        # Act
+        self.connection.reset_timer()
+        # Assert
+        assert (self.connection.timer.is_alive())
+
+    def test_reset_timer_exception(self):
+        # Act
+        self.connection.timeout()
+        with pytest.raises(ConnectionDead):
+            # Assert
+            assert self.connection.reset_timer()
+
+    def test_equality(self):
+        # Arrange/Act
+        conn2 = self.connection
+        # Assert
+        assert conn2 == self.connection
+
+    def test_inequality_same_type(self):
+        # Arrange/Act
+        conn2 = Connection("test_id_2")
+        assert conn2 != self.connection
+
+    def test_inequality_different_type(self):
+        # Arrange
+        conn2 = "str"
+        with pytest.raises(Exception):
+            # Act and Assert
+            assert conn2 != self.connection
--- src/tests/slave_tests/test_heartbeat.py
+++ src/tests/slave_tests/test_heartbeat.py
@@ -1,11 +1,17 @@
+from typing import Type
+
 import pytest
 from requests import Response, Session, exceptions as RequestExceptions
 from unittest.mock import MagicMock
 from time import sleep
+
+from requests.sessions import Session
+
 from slave.heartbeat import Heartbeat
 
 
 class TestHeartbeat:
+    mock_session: Session
     mock_url = 'https://mock_url.ca'
 
     def setup_method(self, method):
@@ -14,7 +20,7 @@ class TestHeartbeat:
         """
         self.mock_response = Response()
         self.mock_response.status_code = 200
-        self.mock_session = Session
+        self.mock_session = Session()
         self.mock_session.get = MagicMock(return_value=self.mock_response)
         self.heartbeat = Heartbeat(self.mock_session, self.mock_url)
 
@@ -55,7 +61,6 @@ class TestHeartbeat:
     def test_retry_attempts(self):
         # Arrange
         self.mock_response.status_code = 500
-        self.mock_session.get = MagicMock(return_value=self.mock_response)
         self.heartbeat = Heartbeat(self.mock_session, self.mock_url, interval_secs=0.0, retry_attempts=0)
         # Act
         self.heartbeat.start_beating()
@@ -63,15 +68,6 @@ class TestHeartbeat:
         # Assert
         assert (self.heartbeat.fails == 1)
         assert (not self.heartbeat.timer.is_alive())
-
-    def test_catch_generic_exception(self):
-        # Arrange
-        self.mock_response = None
-        self.mock_session.get = MagicMock(side_effect=Exception("Mock Error. Ignore Me."))
-        self.heartbeat = Heartbeat(self.mock_session, self.mock_url, interval_secs=0.0, retry_attempts=0)
-        with pytest.raises(Exception):
-            # Act and Assert
-            assert self.heartbeat.start_beating()
 
     def test_catch_generic_exception(self):
         # Arrange
